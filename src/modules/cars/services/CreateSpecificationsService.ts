26697eb4378c51ecaf6402646cb0954b0c3993b9
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2023-02-02 16:21:32 -0300

    create specifications routes

---
+++ src/modules/cars/services/CreateSpecificationsService.ts
@@ -0,0 +1,25 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationsService {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name: simpleName, description }: IRequest): void {
+    const name = simpleName.toUpperCase();
+
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error(`Specification ${name} already exists`);
+    }
+
+    this.specificationsRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationsService };
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecificationsService } from "../modules/cars/services/CreateSpecificationsService";
+
+const specificationsRoutes = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (req, res) => {
+  const { name, description } = req.body;
+
+  const createSpecificationService = new CreateSpecificationsService(
+    specificationsRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return res.status(201).json();
+});
+
+export { specificationsRoutes };
